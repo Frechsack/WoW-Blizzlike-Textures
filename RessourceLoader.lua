3d858dff1e8dfedf9bc7377ac60cda34f8f8d691
Frechsack <patrick.wolter@pwolter.de>
2022-12-09 14:10:42 +0100

    Init

---
+++ RessourceLoader.lua
@@ -0,0 +1,10 @@
+
+local LSM = LibStub("LibSharedMedia-3.0")
+local MediaType_BACKGROUND = LSM.MediaType.BACKGROUND
+local MediaType_BORDER = LSM.MediaType.BORDER
+local MediaType_FONT = LSM.MediaType.FONT
+local MediaType_STATUSBAR = LSM.MediaType.STATUSBAR
+
+LSM:Register(MediaType_STATUSBAR, "Tukui", [[Interface\Addons\RessourceLoader\statusbar\tukui.tga]])
+LSM:Register(MediaType_BORDER, "Glow", [[Interface\Addons\RessourceLoader\border\glow.tga]])
+LSM:Register(MediaType_FONT, "Expressway", [[Interface\Addons\RessourceLoader\font\expressway.ttf]])
