90b57d85c2891d21f68f52b54f8aed681d4deb24
frechsack <patrick.wolter@pwolter.de>
2024-08-01 21:06:02 +0200

    Init

--- RessourceLoader.lua
+++ RessourceLoader.lua
@@ -5,6 +5,20 @@ local MediaType_BORDER = LSM.MediaType.BORDER
 local MediaType_FONT = LSM.MediaType.FONT
 local MediaType_STATUSBAR = LSM.MediaType.STATUSBAR
 
-LSM:Register(MediaType_STATUSBAR, "Tukui", [[Interface\Addons\RessourceLoader\statusbar\tukui.tga]])
+
+LSM:Register(MediaType_STATUSBAR, "BlizzQuestTrackerHeadline", [[Interface\Addons\RessourceLoader\statusbar\BlizzQuestTrackerHeadline.blp]])
+LSM:Register(MediaType_STATUSBAR, "BlizzQuestTrackerLineHighlight", [[Interface\Addons\RessourceLoader\statusbar\BlizzQuestTrackerLineHighlight.blp]])
+LSM:Register(MediaType_STATUSBAR, "BlizzPortraitFrameRound", [[Interface\Addons\RessourceLoader\statusbar\BlizzPortraitFrameRound2.blp]])
+
+LSM:Register(MediaType_STATUSBAR, "BlizzUfBar", [[Interface\Addons\RessourceLoader\statusbar\BlizzUfBar.blp]])
+LSM:Register(MediaType_STATUSBAR, "BlizzUfBorderBottom", [[Interface\Addons\RessourceLoader\statusbar\BlizzUfBorderBottom.blp]])
+LSM:Register(MediaType_STATUSBAR, "BlizzUfBorderLeft", [[Interface\Addons\RessourceLoader\statusbar\BlizzUfBorderLeft.blp]])
+LSM:Register(MediaType_STATUSBAR, "BlizzUfBorderMiddle", [[Interface\Addons\RessourceLoader\statusbar\BlizzUfBorderMiddle.blp]])
+LSM:Register(MediaType_STATUSBAR, "BlizzUfBorderRight", [[Interface\Addons\RessourceLoader\statusbar\BlizzUfBorderRight.blp]])
+LSM:Register(MediaType_STATUSBAR, "BlizzUfBorderTop", [[Interface\Addons\RessourceLoader\statusbar\BlizzUfBorderTop.blp]])
+LSM:Register(MediaType_STATUSBAR, "BlizzUfBarCarbon", [[Interface\Addons\RessourceLoader\statusbar\BlizzUfBarCarbon.blp]])
+
+
 LSM:Register(MediaType_BORDER, "Glow", [[Interface\Addons\RessourceLoader\border\glow.tga]])
+LSM:Register(MediaType_BORDER, "BlizzardTooltipBorder", [[Interface\Addons\RessourceLoader\border\BlizzardTooltipBorder.blp]])
 LSM:Register(MediaType_FONT, "Expressway", [[Interface\Addons\RessourceLoader\font\expressway.ttf]])
