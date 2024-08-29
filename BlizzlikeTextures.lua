915bed17b6ab7e210326cf9a6767b6849e0145a0
frechsack <patrick.wolter@pwolter.de>
2024-08-29 22:01:32 +0200

    Init

--- BlizzlikeTextures.lua
+++ BlizzlikeTextures.lua
@@ -6,20 +6,20 @@ local MediaType_FONT = LSM.MediaType.FONT
 local MediaType_STATUSBAR = LSM.MediaType.STATUSBAR
 
 
-LSM:Register(MediaType_STATUSBAR, "BlizzQuestTrackerHeadline", [[Interface\Addons\RessourceLoader\statusbar\BlizzQuestTrackerHeadline.blp]])
-LSM:Register(MediaType_STATUSBAR, "BlizzQuestTrackerLineHighlight", [[Interface\Addons\RessourceLoader\statusbar\BlizzQuestTrackerLineHighlight.blp]])
-LSM:Register(MediaType_STATUSBAR, "BlizzPortraitFrameRound", [[Interface\Addons\RessourceLoader\statusbar\BlizzPortraitFrameRound2.blp]])
+LSM:Register(MediaType_STATUSBAR, "BlizzQuestTrackerHeadline", [[Interface\Addons\BlizzlikeTextures\statusbar\BlizzQuestTrackerHeadline.blp]])
+LSM:Register(MediaType_STATUSBAR, "BlizzQuestTrackerLineHighlight", [[Interface\Addons\BlizzlikeTextures\statusbar\BlizzQuestTrackerLineHighlight.blp]])
+LSM:Register(MediaType_STATUSBAR, "BlizzPortraitFrameRound", [[Interface\Addons\BlizzlikeTextures\statusbar\BlizzPortraitFrameRound2.blp]])
 
-LSM:Register(MediaType_STATUSBAR, "BlizzUfBar", [[Interface\Addons\RessourceLoader\statusbar\BlizzUfBar.blp]])
-LSM:Register(MediaType_STATUSBAR, "BlizzUfBorderBottom", [[Interface\Addons\RessourceLoader\statusbar\BlizzUfBorderBottom.blp]])
-LSM:Register(MediaType_STATUSBAR, "BlizzUfBorderLeft", [[Interface\Addons\RessourceLoader\statusbar\BlizzUfBorderLeft.blp]])
-LSM:Register(MediaType_STATUSBAR, "BlizzUfBorderMiddle", [[Interface\Addons\RessourceLoader\statusbar\BlizzUfBorderMiddle.blp]])
-LSM:Register(MediaType_STATUSBAR, "BlizzUfBorderRight", [[Interface\Addons\RessourceLoader\statusbar\BlizzUfBorderRight.blp]])
-LSM:Register(MediaType_STATUSBAR, "BlizzUfBorderTop", [[Interface\Addons\RessourceLoader\statusbar\BlizzUfBorderTop.blp]])
-LSM:Register(MediaType_STATUSBAR, "BlizzUfBarCarbon", [[Interface\Addons\RessourceLoader\statusbar\BlizzUfBarCarbon.blp]])
-LSM:Register(MediaType_STATUSBAR, "ElvUI", [[Interface\Addons\RessourceLoader\statusbar\ElvUI.tga]])
+LSM:Register(MediaType_STATUSBAR, "BlizzUfBar", [[Interface\Addons\BlizzlikeTextures\statusbar\BlizzUfBar.blp]])
+LSM:Register(MediaType_STATUSBAR, "BlizzUfBorderBottom", [[Interface\Addons\BlizzlikeTextures\statusbar\BlizzUfBorderBottom.blp]])
+LSM:Register(MediaType_STATUSBAR, "BlizzUfBorderLeft", [[Interface\Addons\BlizzlikeTextures\statusbar\BlizzUfBorderLeft.blp]])
+LSM:Register(MediaType_STATUSBAR, "BlizzUfBorderMiddle", [[Interface\Addons\BlizzlikeTextures\statusbar\BlizzUfBorderMiddle.blp]])
+LSM:Register(MediaType_STATUSBAR, "BlizzUfBorderRight", [[Interface\Addons\BlizzlikeTextures\statusbar\BlizzUfBorderRight.blp]])
+LSM:Register(MediaType_STATUSBAR, "BlizzUfBorderTop", [[Interface\Addons\BlizzlikeTextures\statusbar\BlizzUfBorderTop.blp]])
+LSM:Register(MediaType_STATUSBAR, "BlizzUfBarCarbon", [[Interface\Addons\BlizzlikeTextures\statusbar\BlizzUfBarCarbon.blp]])
+LSM:Register(MediaType_STATUSBAR, "ElvUI", [[Interface\Addons\BlizzlikeTextures\statusbar\ElvUI.tga]])
 
 
-LSM:Register(MediaType_BORDER, "Glow", [[Interface\Addons\RessourceLoader\border\glow.tga]])
-LSM:Register(MediaType_BORDER, "BlizzardTooltipBorder", [[Interface\Addons\RessourceLoader\border\BlizzardTooltipBorder.blp]])
-LSM:Register(MediaType_FONT, "Expressway", [[Interface\Addons\RessourceLoader\font\expressway.ttf]])
+LSM:Register(MediaType_BORDER, "Glow", [[Interface\Addons\BlizzlikeTextures\border\glow.tga]])
+LSM:Register(MediaType_BORDER, "BlizzardTooltipBorder", [[Interface\Addons\BlizzlikeTextures\border\BlizzardTooltipBorder.blp]])
+LSM:Register(MediaType_FONT, "Expressway", [[Interface\Addons\BlizzlikeTextures\font\expressway.ttf]])
